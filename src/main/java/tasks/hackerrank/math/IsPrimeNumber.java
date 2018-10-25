004d549d9dd9e02f1f244dde9e3eda1bfff067f8
vermucht <vermucht@gmail.com>
2018-10-25 13:55:58 +0300

    Ok, it's multireturn, but it's also self-explanatory

--- src/main/java/tasks/hackerrank/math/IsPrimeNumber.java
+++ src/main/java/tasks/hackerrank/math/IsPrimeNumber.java
@@ -24,22 +24,24 @@ public class IsPrimeNumber {
      * First, we check if n is divisible by 2 using bitwise operation: n & 1.
      * If not, we take step == 2 and check from 3 to sqrt(n)
      * <p>
-     * The only improvement iof this method can be if we check division
+     * The only improvement of this method can be if we check division
      * by ALL PRIMES only, from 2 to sqrt(n). But we don't know them.
      *
      * @param n Given number.
      * @return true if prime, false if not.
      */
     public boolean oneOfBestWays(int n) {
+        if (n == 2) {
+            return true;
+        }
+        if (n == 1 || ((n & 1) == 0)) {
+            return false;
+        }
         boolean result = true;
-        if (n != 2 && (n == 1 || (n & 1) == 0)) {
-            result = false;
-        } else {
-            for (int i = 3; i <= Math.sqrt(n); i += 2) {
-                if (n % i == 0) {
-                    result = false;
-                    break;
-                }
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                result = false;
+                break;
             }
         }
         return result;
