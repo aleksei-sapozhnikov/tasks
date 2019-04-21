42137b0fed5c8ebcea7bbc848c435c65dfcbd263
vermucht <vermucht@gmail.com>
2019-04-21 19:04:09 +0300

    Добавил тест на graphNumber

--- src/test/java/tasks/algorithm/Array2dToMatrixTest.java
+++ src/test/java/tasks/algorithm/Array2dToMatrixTest.java
@@ -31,6 +31,20 @@ public class Array2dToMatrixTest {
     }
 
     @Test
+    public void graphNumber() {
+        int[][] array = {
+                {1, 3, 6},
+                {6, 4, 5}
+        };
+        assertEquals(0, this.convert.graphNumber(array, new int[]{0, 0}));
+        assertEquals(1, this.convert.graphNumber(array, new int[]{0, 1}));
+        assertEquals(2, this.convert.graphNumber(array, new int[]{0, 2}));
+        assertEquals(3, this.convert.graphNumber(array, new int[]{1, 0}));
+        assertEquals(4, this.convert.graphNumber(array, new int[]{1, 1}));
+        assertEquals(5, this.convert.graphNumber(array, new int[]{1, 2}));
+    }
+
+    @Test
     public void cellCoords() {
         int[][] array = {
                 {1, 3, 6},
